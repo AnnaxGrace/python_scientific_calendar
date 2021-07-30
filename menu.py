6ce2404ba3adaf63db02ccfc17c94fe15f8a8b59
Conover, Anna <aconover@teksystems.com>
2021-07-30 09:25:15 -0500

    added square root, power, and logorithm

--- menu.py
+++ menu.py
@@ -1,16 +1,28 @@
 import math
 
 def do_math(operator):
-    val1 = float(input("\nFirst value: "))
-    val2 = float(input("\nSecond value: "))
-    if operator == "0":
-        final = val1 + val2
-    elif operator == "1":
-        final = val1 - val2
-    elif operator == "2":
-        final = val1 * val2
-    elif operator == "3":
-        final = val1 / val2
+    if operator == "6":
+        val1 = float(input("\nEnter value for extracting the square root: "))
+        final = math.sqrt(val1)
+    elif operator == "7":
+        val1 = float(input("\nEnter value for calculating the logarithm to base 2: "))
+        final = math.log(val1, 2)
+    else:
+        val1 = float(input("\nFirst value: "))
+        val2 = float(input("\nSecond value: "))
+        if operator == "0":
+            final = val1 + val2
+        elif operator == "1":
+            final = val1 - val2
+        elif operator == "2":
+            final = val1 * val2
+        elif operator == "3":
+            final = val1 / val2
+        elif operator == "4":
+            final = val1 % val2
+        elif operator == "5":
+            final = math.pow(val1, val2)
+
     return "\nThe result is: " + str(final) + "\n"
 
 def back_to_menu():
